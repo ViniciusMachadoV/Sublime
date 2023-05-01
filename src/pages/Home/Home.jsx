cefa2f41b9eef75171ad3588aca7e2a0d1485af0
Emanuel Andrade <emanuelpna@yahoo.com.br>
2023-05-01 19:21:44 -0300

    criado página inicial

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
 import styles from "./Home.module.css";
 
 function Home() {
-  return <h1 className={styles.homePageStyles}>Home</h1>;
+  return (
+    <div className="container w-50 mt-5 pt-5 mx-auto d-flex flex-row justify-content-between">
+      <div className="col-md-4 d-flex flex-column justify-content-end">
+        <strong className={`row ${styles.tituloHome}`}>sublime</strong>
+
+        <span className={`row ${styles.subtituloHome}`}>
+          A handcrafted collection for the new season to come
+        </span>
+      </div>
+
+      <div className="col-md-6 d-flex justify-content-end">
+        <img
+          className={styles.imagemCoverHome}
+          src="/assets/cover-1.jpg"
+          alt="Modelo com cabelo curto e óculos de sol preto no rosto olhando na diagonal para baixo, usando uma blusa branca e com um paletó marrom vestido em apenas um dos braços"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
