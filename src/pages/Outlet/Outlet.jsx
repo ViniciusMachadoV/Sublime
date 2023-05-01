f4e684d0f00d1a4cbeded7319d032eb21d202546
Emanuel Andrade <emanuelpna@yahoo.com.br>
2023-05-01 19:20:39 -0300

    criadas páginas de listagem de produtos

--- src/pages/Outlet/Outlet.jsx
+++ src/pages/Outlet/Outlet.jsx
@@ -1,7 +1,54 @@
-import styles from "./Outlet.module.css";
+import TituloPagina from "../../components/commons/TituloPagina/TituloPagina";
+import ListaProdutos from "../../components/commons/ListaProdutos/ListaProdutos";
 
 function Outlet() {
-  return <h1 className={styles.examplePageStyles}>Outlet</h1>;
+  const produtos = [
+    {
+      nome: "Nome do Produto 1",
+      img: "/assets/cover-1.jpg",
+      preco: 4200,
+    },
+    {
+      nome: "Nome do Produto 2",
+      img: "/assets/cover-1.jpg",
+      preco: 4200,
+    },
+    {
+      nome: "Nome do Produto 3",
+      img: "/assets/cover-1.jpg",
+      preco: 4200,
+    },
+    {
+      nome: "Nome do Produto 4",
+      img: "/assets/cover-1.jpg",
+      preco: 4200,
+    },
+    {
+      nome: "Nome do Produto 5",
+      img: "/assets/cover-1.jpg",
+      preco: 4200,
+    },
+    {
+      nome: "Nome do Produto 6",
+      img: "/assets/cover-1.jpg",
+      preco: 4200,
+    },
+  ];
+
+  return (
+    <section className="container">
+      <header className="d-flex justify-content-between align-items-end">
+        <TituloPagina nome="Outlet" />
+
+        <div>
+          <span className="fs-5">Oferta acaba em: </span>
+          <span className="fs-3">10:23:12</span>
+        </div>
+      </header>
+
+      <ListaProdutos produtos={produtos} />
+    </section>
+  );
 }
 
 export default Outlet;
