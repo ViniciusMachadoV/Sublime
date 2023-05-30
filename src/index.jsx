84040caeb7b5f974dd7ab5b46d5e085a9e927952
Emanuel Andrade <emanuelpna@yahoo.com.br>
2023-05-30 18:11:15 -0300

    adicionando tema e estilos iniciais

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import "./styles/reset.css";
 import "./styles/index.css";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/sublime-boostrap.scss";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
