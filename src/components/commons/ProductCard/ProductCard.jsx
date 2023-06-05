7ca9e1647217f0428d5c8127bb91201e0d7e9e9f
Emanuel Andrade <emanuelpna@yahoo.com.br>
2023-06-05 11:32:42 -0300

    reduce font size of ProductCard name and price

--- src/components/commons/ProductCard/ProductCard.jsx
+++ src/components/commons/ProductCard/ProductCard.jsx
@@ -1,23 +1,24 @@
-import styles from './ProductCard.module.css';
+import styles from "./ProductCard.module.css";
 
-function ProductCard(props){
-    return(
-        <section className={styles.productContainer}>
-            <div className={styles.productCardImgContainer}>
-                <img
-                className={styles.productCardImage}
-                src={props.image}
-                alt={props.name}
-                />
-                <button className={styles.productCardButton}>Adicionar ao Carrinho</button>
-            </div>
-            <div className={styles.productCardInfo}>
-                <stron className={styles.productCardName}>{props.name}</stron>
-                <span className={styles.productCardPrice}>{props.price} €</span>
-            </div>
-            
-        </section>
-    )
+function ProductCard(props) {
+  return (
+    <section className={styles.productContainer}>
+      <div className={styles.productCardImgContainer}>
+        <img
+          className={styles.productCardImage}
+          src={props.image}
+          alt={props.name}
+        />
+        <button className={styles.productCardButton}>
+          Adicionar ao Carrinho
+        </button>
+      </div>
+      <div className={styles.productCardInfo}>
+        <stron className={styles.productCardName}>{props.name}</stron>
+        <span className={styles.productCardPrice}>{props.price} €</span>
+      </div>
+    </section>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
