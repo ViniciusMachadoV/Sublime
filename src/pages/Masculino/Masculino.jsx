34622c8633ff98012f3d64f06604d6fcd172fa4e
Emanuel Andrade <emanuelpna@yahoo.com.br>
2023-06-18 15:32:33 -0300

    usando fetch para popular as telas de produtos

--- src/pages/Masculino/Masculino.jsx
+++ src/pages/Masculino/Masculino.jsx
@@ -1,14 +1,21 @@
+import { masculineProductsRepository } from "../../data/useCases/ProductsRepository";
+
+import { useFetch } from "../../infra/hooks/useFetch";
+
 import { ProductsPage } from "../../components/layout/ProductsPage/ProductsPage";
 
+import { Loader } from "../../components/commons/Loader/Loader";
 import ListaProdutos from "../../components/commons/ListaProdutos/ListaProdutos";
 import { ProductsHeroSection } from "../../components/commons/ProductsHeroSection/ProductsHeroSection";
 
-import { produtos } from "../../data/produtos";
-
 function Masculino() {
+  const { data: produtos, isFetching } = useFetch(() =>
+    masculineProductsRepository.getAll()
+  );
+
   return (
     <ProductsPage header={<ProductsHeroSection title="Masculino" />}>
-      <ListaProdutos produtos={produtos} />
+      {isFetching ? <Loader /> : <ListaProdutos produtos={produtos ?? []} />}
     </ProductsPage>
   );
 }
