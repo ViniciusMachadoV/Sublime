97d5d37ee86f5539d9f1731a1a755cd2004a9324
Emanuel Andrade <emanuelpna@yahoo.com.br>
2023-06-21 19:27:38 -0300

    corrigido título não mudando junto da troca de temas

--- src/pages/Home/parts/DivaSection/DivaSection.jsx
+++ src/pages/Home/parts/DivaSection/DivaSection.jsx
@@ -4,28 +4,43 @@ import { ThemeContext } from "../../../../infra/contexts/ThemeContext";
 
 import styles from "./DivaSection.module.css";
 
-const photos = {
-  summer: ["summerclose.png", "summerhorizontal.png", "summervertical.png"],
-  spring: [
-    "standmodelspring.jpg",
-    "springhorizontalpic.jpg",
-    "cameramodelspring.jpg",
-  ],
-  winter: [
-    "hunterclosewinter.jpg",
-    "hunterlayedwinter.jpg",
-    "hunterstandwinter.png",
-  ],
-  autumn: ["cover-1.jpg", "divasection-2.jpg", "divasection-3.jpg"],
-  default: ["cover-1.jpg", "divasection-2.jpg", "divasection-3.jpg"],
+const SectionThemes = {
+  summer: {
+    title: "Summer",
+    photos: ["summerclose.png", "summerhorizontal.png", "summervertical.png"],
+  },
+  spring: {
+    title: "Spring",
+    photos: [
+      "standmodelspring.jpg",
+      "springhorizontalpic.jpg",
+      "cameramodelspring.jpg",
+    ],
+  },
+  winter: {
+    title: "Winter",
+    photos: [
+      "hunterclosewinter.jpg",
+      "hunterlayedwinter.jpg",
+      "hunterstandwinter.png",
+    ],
+  },
+  autumn: {
+    title: "Autumn",
+    photos: ["cover-1.jpg", "divasection-2.jpg", "divasection-3.jpg"],
+  },
+  default: {
+    title: "Autumn",
+    photos: ["cover-1.jpg", "divasection-2.jpg", "divasection-3.jpg"],
+  },
 };
 
 function DivaSection() {
   const { theme } = useContext(ThemeContext);
 
-  const sectionPhotos = theme
-    ? photos[theme] ?? photos.default
-    : photos.default;
+  const sectionTheme = theme
+    ? SectionThemes[theme] ?? SectionThemes.default
+    : SectionThemes.default;
 
   return (
     <div className="mx-auto d-flex flex-row justify-content-between">
@@ -35,24 +50,24 @@ function DivaSection() {
         </span>
 
         <strong className={`row ${styles.tituloHome}`}>
-          Winter Collection
+          {sectionTheme.title} Collection
         </strong>
       </div>
 
       <div className={`col-md-6 ${styles.divaSectionImages}`}>
         <img
           className={styles.imagemCoverHome}
-          src={`/assets/${sectionPhotos[0]}`}
+          src={`/assets/${sectionTheme.photos[0]}`}
           alt="Um close no rosto da Hunter mostrando as joias nas mãos."
         />
         <img
           className={styles.horizontalCoverImage}
-          src={`/assets/${sectionPhotos[1]}`}
+          src={`/assets/${sectionTheme.photos[1]}`}
           alt="Hunter deitada usando um bodysuit azul."
         />
         <img
           className={styles.verticalCoverImage}
-          src={`/assets/${sectionPhotos[2]}`}
+          src={`/assets/${sectionTheme.photos[2]}`}
           alt="A mulher mais linda do mundo em pé um pouco de lado olhando em direção à camera usando um bodysuit azul"
         />
       </div>
